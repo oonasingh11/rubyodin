53a707b9b805d8972b5a7082e1f35abe5f05bfee
Oona Singh <oonarsingh@gmail.com>
2022-12-29 16:47:32 +0400

    stock_picker modified and bubble_sort file created

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,15 +1,16 @@
 def stock_picker(prices)
   # variables to store the best buy and sell days
+  # set variables to 0
   best_buy_day = 0
   best_sell_day = 0
-  # Initialize a variable to store the maximum profit
+  # variable to store the maximum profit, also set to 0
   max_profit = 0
 
-  # Iterate over each day - except the last
+  # Iterate over each day - except the last as not possible to sell on same day it is bought
   (0...prices.length - 1).each do |buy_day|
     # Iterate over each day after the current buy day
     (buy_day + 1...prices.length).each do |sell_day|
-      # Calculate the profit for the current buy and sell days
+      # Profit for current buy and sell days
       profit = prices[sell_day] - prices[buy_day]
       # If the profit is greater than the current maximum profit,
       # update the best buy and sell days and the maximum profit
