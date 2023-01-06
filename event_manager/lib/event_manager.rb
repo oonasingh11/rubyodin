759dfc1bafc7ed7d7d19ea712884a230f469bf0d
Oona Singh <oonarsingh@gmail.com>
2023-01-06 10:54:43 +0400

    zipcodes added

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,3 +1,4 @@
+require 'csv'
 puts 'Event Manager Initialized!'
 
 lines = File.readlines('event_attendees.csv')
@@ -7,4 +8,17 @@ lines.each_with_index do |line, index|
   columns = line.split(",")
   name = columns[2]
   puts name
+end
+
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
