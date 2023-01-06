64e4f52a9d2d88dcbe974ee529ff969dde8a0814
Oona Singh <oonarsingh@gmail.com>
2023-01-06 10:48:10 +0400

    each with index used instead to skip header

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,7 +1,9 @@
 puts 'Event Manager Initialized!'
 
 lines = File.readlines('event_attendees.csv')
-lines.each do |line|
+lines.each_with_index do |line, index|
+  next if index.zero?
+
   columns = line.split(",")
   name = columns[2]
   puts name
